9c17dec225f4418937754080334c1e3d5823205b
xwhiteclouds <anano.kuchava2006@gmail.com>
2021-04-24 16:10:58 +0400

    api1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import Books from './components/Books'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 export default function App() {
   return (
-    <div>
-      <Books />
-    </div>
+    <Router>
+      <Link to='/'><button>books</button></Link> 
+      <Link to='/auth'><button>sign in</button></Link> 
+      <Switch>
+        <Route path='/' exact>
+          <Books/>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
